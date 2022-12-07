4f8520ffab2a71fe70cf8ee74eecc0fafd09daef
liuyue <liuyue@youku.com>
2022-12-07 16:25:55 +0800

    fix header token

--- app/base.py
+++ app/base.py
@@ -11,7 +11,7 @@ class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         print("setting headers!!!")
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE')
 
     def render(self,template_name,**kwargs):
